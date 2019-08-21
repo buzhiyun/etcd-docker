7ae699ed8045d759b563a123f4f3c9f60f376d76
周杨 <buzhiyun@sina.com>
2019-08-21 09:11:09 +0800

    更新用环境变量启动

--- etcd_run.sh
+++ etcd_run.sh
@@ -14,5 +14,42 @@
 #if [ $ETCD_INITIAL_CLUSTER ];then
 #  ARGS=$ARGS" --initial-cluster \"$ETCD_INITIAL_CLUSTER\""
 #  echo "set clusteUrl $CLUSTE_URL" 
-#fi          
-/etcd/etcd --name $(hostname) --data-dir '/data' --listen-peer-urls "http://0.0.0.0:2380" --listen-client-urls "http://0.0.0.0:2379,http://0.0.0.0:4001"  --advertise-client-urls "http://$(hostname -i):2379" --initial-advertise-peer-urls "http://$(hostname -i):2380"
+#fi 
+
+# 配置环境变量  参见 https://github.com/etcd-io/etcd/blob/master/Documentation/op-guide/configuration.md
+         
+if [ 0"$ETCD_NAME" = "0" ];then
+  export ETCD_NAME=$(hostname)
+fi
+
+
+if [ 0"$ETCD_DATA_DIR" = "0" ];then
+  export ETCD_DATA_DIR="/data"
+fi 
+
+
+if [ 0"$ETCD_LISTEN_PEER_URLS" = "0" ];then
+  export ETCD_LISTEN_PEER_URLS="http://0.0.0.0:2380"
+fi
+
+
+if [ 0"$ETCD_LISTEN_CLIENT_URLS" = "0" ];then
+  export ETCD_LISTEN_CLIENT_URLS="http://0.0.0.0:2379,http://0.0.0.0:4001"
+fi
+
+
+if [ 0"$ETCD_INITIAL_ADVERTISE_PEER_URLS" = "0" ];then
+  export ETCD_INITIAL_ADVERTISE_PEER_URLS="http://$(hostname -i):2380"
+fi
+
+
+if [ 0"$ETCD_ADVERTISE_CLIENT_URLS" = "0" ];then
+  export ETCD_ADVERTISE_CLIENT_URLS="http://$(hostname -i):2379"
+fi
+
+
+if [ 0"$ETCD_ENABLE_V2" = "0" ];then
+  export ETCD_ENABLE_V2=true
+fi
+
+/etcd/etcd 
